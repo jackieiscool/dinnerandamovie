4078c5d31c573125faa6e02dad41e34f7d479aea
Kristine <screamingmunch@gmail.com>
2014-02-22 15:06:17 -0800

    seed file restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,20 @@
 
 
 restaurants = Restaurant.create([
-  { name: '', description: '', head_chef: '', city: '', country_display: '', country_code: '', price: , website: '', lat: , lng: ''},
-  { }
+
+  { id: 1,  name: 'El Celler de Can Roca', description: 'Modern Spanish cuisine.', head_chef: 'Joan Roca', city: 'Girona',airport_code: 'GRO', country_display: 'Spain', country_code: 'ESP', price: 150 , website: 'www.cellercanroca.com', lat: 41.993144 , lng: 2.80793 },
+  { id: 2,  name: 'Noma', description: "Mould-breaking Nordic food that takes nature’s bounty to new levels", head_chef: 'René Redzepi', city: 'Copenhagen',airport_code: 'CPH', country_display: 'Denmark', country_code: 'DNK', price: 210, website: 'http://noma.dk/', lat: 55.677933, lng: 12.596201 },
+  { id: 3,  name: 'Osteria Francescana', description: "Modern Italian cuisine ", head_chef: 'Massimo Bottura', city: 'Modena',airport_code: 'BLQ', country_display: 'Italy', country_code: '', price: , website: 'http://www.osteriafrancescana.it', lat: 44.644802 , lng: 10.921574 },
+  # { id: 4,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 5,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 6,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 7,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 8,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 9,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+  # { id: 10, name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
+
   ])
+
 departures = Departure.create([
   { city: 'San Francisco', state: 'CA', country: 'USA', airport_code: 'SFO' }
   ])
-
