a8f5b7f0f35a67979c8a25683800c42f9e33e9e0
Sara LaFassett <sara@lafassett.com>
2014-02-22 14:11:25 -0800

    Fixing new migration.

--- db/migrate/20140222215057_create_departures.rb
+++ db/migrate/20140222215057_create_departures.rb
@@ -1,9 +1,9 @@
 class CreateDepartures < ActiveRecord::Migration
   def change
     create_table :departures do |t|
-      t.string :city,
-      t.string :state,
-      t.string :country,
+      t.string :city
+      t.string :state
+      t.string :country
       t.string :airport_code
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140222181724) do
+ActiveRecord::Schema.define(:version => 20140222215057) do
+
+  create_table "departures", :force => true do |t|
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "airport_code"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "restaurants", :force => true do |t|
     t.string   "name"
