2089ce818df62a0496cc2dc4a26705cbe091750e
Kristine <screamingmunch@gmail.com>
2014-02-21 21:58:04 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DinnerMovieApp::Application.config.secret_token = '61937b6c009c4928823ec5737b72778959a19b6b95dcb9edfb8b2f5d98e480cffe18a5b341971bfa59632884a5df8366e435d80ba44eca6e032d8e970e81b36e'
