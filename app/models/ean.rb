ca97fbd1a12b0cd841b98c4e827dafefbbcd97b3
Sara LaFassett <sara@lafassett.com>
2014-02-22 15:33:27 -0800

    Configuring api endpoint for angular.

--- app/models/ean.rb
+++ app/models/ean.rb
@@ -7,20 +7,20 @@ class Ean
     data = []
     restaurants = Restaurant.all
     restaurants.each do |restaurant|
-      hotel = Expedia.hotel city
-      restaurant.hotel =
+      hotel = self.hotel(restaurant)
+      restaurant[:hotel] =
       { name: hotel["name"],
         price: hotel["highRate"]
       }
-      restaurant.flight = 200
+      restaurant[:flight] = 200
       data << restaurant
     end
     data
   end
 
-  def self.hotel(city)
+  def self.hotel(restaurant)
     api = Expedia::Api.new
-    response = api.get_list({city: city, sort: "QUALITY", NumberOfResults: 1})
+    response = api.get_list({latitude: restaurant.lat, longitude: restaurant.lng, sort: "QUALITY", NumberOfResults: 1})
     if response.exception?
       false
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 
 restaurants = Restaurant.create([
 
-  { name: 'El Celler de Can Roca', description: 'Modern Spanish cuisine.', head_chef: 'Joan Roca', city: 'Girona',airport_code: 'GRO', country_display: 'Spain', country_code: 'ESP', price: 150 , website: 'www.cellercanroca.com', lat: 41.993144 , lng: 2.80793 },
-  { name: 'Noma', description: "Mould-breaking Nordic food that takes nature’s bounty to new levels", head_chef: 'René Redzepi', city: 'Copenhagen',airport_code: 'CPH', country_display: 'Denmark', country_code: 'DNK', price: 210, website: 'http://noma.dk/', lat: 55.677933, lng: 12.596201 },
-  { name: 'Osteria Francescana', description: "Modern Italian cuisine ", head_chef: 'Massimo Bottura', city: 'Modena',airport_code: 'BLQ', country_display: 'Italy', country_code: '', price: 200, website: 'http://www.osteriafrancescana.it', lat: 44.644802 , lng: 10.921574 },
+  { name: 'El Celler de Can Roca', description: 'Modern Spanish cuisine.', head_chef: 'Joan Roca', city: 'Girona',airport_code: 'GRO', country_display: 'Spain', country_code: 'ES', price: 150 , website: 'www.cellercanroca.com', lat: 41.993144 , lng: 2.80793 },
+  { name: 'Noma', description: "Mould-breaking Nordic food that takes nature’s bounty to new levels", head_chef: 'René Redzepi', city: 'Copenhagen', airport_code: 'CPH', country_display: 'Denmark', country_code: 'DK', price: 210, website: 'http://noma.dk/', lat: 55.677933, lng: 12.596201 },
+  { name: 'Osteria Francescana', description: "Modern Italian cuisine ", head_chef: 'Massimo Bottura', city: 'Modena',airport_code: 'BLQ', country_display: 'Italy', country_code: 'IT', price: 200, website: 'http://www.osteriafrancescana.it', lat: 44.644802 , lng: 10.921574 },
   # { id: 4,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
   # { id: 5,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
   # { id: 6,  name: '', description: " ", head_chef: '', city: '',airport_code: '', country_display: '', country_code: '', price: , website: '', lat: , lng: },
